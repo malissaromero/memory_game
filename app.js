0188dd3998f97074f22b76464ec2f4f2856df965
Malissa Romero <romero.malissa@gmail.com>
2016-04-23 10:45:47 -0400

    server set up, not linked with rest of app

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
-}).listen(1300, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1300/');
+}).listen(3000, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:3000/');
